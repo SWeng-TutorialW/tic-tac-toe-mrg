02e09c0aeb018d81bc3eeb7f7bec86f8fe5e3ffe
Micheal <michealghanadre@gmail.com>
2025-05-04 16:00:09 +0300

    Build the Tic Tac Toe

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -1,29 +1,61 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-import java.io.IOException;
+/**
+ * Sample Skeleton for 'primary.fxml' Controller Class
+ */
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
 
-    @FXML
-    void sendWarning(ActionEvent event) {
-    	try {
-			SimpleClient.getClient().sendToServer("#warning");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
+	@FXML // fx:id="TEXT"
+	private Label TEXT; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button1"
+	private Button button1; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button2"
+	private Button button2; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button3"
+	private Button button3; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button4"
+	private Button button4; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button5"
+	private Button button5; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button6"
+	private Button button6; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button7"
+	private Button button7; // Value injected by FXMLLoader
+
+	@FXML // fx:id="button8"
+	private Button button8; // Value injected by FXMLLoader
+
+	@FXML // fx:id="startButton"
+	private Button startButton; // Value injected by FXMLLoader
 
 	@FXML
-	void initialize(){
-		try {
-			SimpleClient.getClient().sendToServer("add client");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	void START(ActionEvent event) {
+
 	}
+
+	@FXML
+	void WriteText(MouseEvent event) {
+
+	}
+
+	@FXML
+	void XO(ActionEvent event) {
+
+	}
+
 }
+
